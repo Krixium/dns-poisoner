174416c01724155ff3b7780b317b5215e3e5be2f
Data Comm <dc@milliways.bcit.ca>
2019-11-10 11:54:10 -0800

    Fast dns header crafting is a meme

--- dns.h
+++ dns.h
@@ -63,17 +63,16 @@ inline int forgeDns(const dnshdr *dnsHeader, const struct in_addr *fakeAddr,
     unsigned char *query = (unsigned char *)((unsigned char *)dnsHeader + sizeof(dnshdr));
 
     ((dnshdr *)output)->id = dnsHeader->id;
-    ((dnshdr *)output)->qcount = htons(1);
-    ((dnshdr *)output)->ancount = htons(1);
-    ((dnshdr *)output)->nscount = htons(0);
-    ((dnshdr *)output)->adcount = htons(0);
+    ((dnshdr *)output)->qcount = 0x0100;
+    ((dnshdr *)output)->ancount = 0x0100;
+    ((dnshdr *)output)->nscount = 0x0000;
+    ((dnshdr *)output)->adcount = 0x0000;
     // flags
     output[2] = 0x81;
     output[3] = 0x80;
 
     // first get the total size of the query, name + class + type
-    for (size = 0; query[size] != 0; size++)
-        ;
+    for (size = 0; query[size] != 0; size++);
     // set the correct total size of the query
     // 2 for type, 2 for class, 1 for null byte
     size += 2 + 2 + 1;
@@ -85,22 +84,8 @@ inline int forgeDns(const dnshdr *dnsHeader, const struct in_addr *fakeAddr,
     size += sizeof(dnshdr);
 
     // create the dns answer
-    // pointer to q name
-    memcpy(output + size, "\xc0\x0c", 2);
-    size += 2;
-    // type
-    memcpy(output + size, "\x00\x01", 2);
-    size += 2;
-    // class
-    memcpy(output + size, "\x00\x01", 2);
-    size += 2;
-    // TTL
-    memcpy(output + size, "\x00\x00\x00\x22", 4);
-    size += 4;
-    // data length
-    memcpy(output + size, "\x00\x04", 2);
-    size += 2;
-    // data
+    memcpy(output + size, "\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\x22\x00\x04", 12);
+    size += 12;
     memcpy(output + size, fakeAddr, 4);
     size += 4;
 
